bfd35e639978b4712e811756ea431e2d96f51ef1
Kim Jun Hyun <junyf00300@gmail.com>
2022-09-18 15:55:38 +0900

    2020180048 김준현
    2020180048 김준현

---
+++ 02/drill4.py
@@ -0,0 +1,73 @@
+Python 3.10.6 (tags/v3.10.6:9c7b4bd, Aug  1 2022, 21:53:49) [MSC v.1932 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+import turtle
+turtle.forward(500)
+turtle.right(90)
+turtle.forward(500)
+turtle.forward(500)
+turtle.right(90)
+turtle.forward(500)
+turtle.right(90)
+turtle.forward(500)
+turtle.penup()
+turtle.goto(0,0)
+turtle.pendown()
+turtle.goto(100,100)
+turtle.undo()
+turtle.goto(0,-100)
+turtle.right(90)
+turtle.goto(500,-100)
+turtle.penup()
+turtle.goto(0,-200)
+turtle.goto(500,-200)
+turtle.undo()
+turtle.pendown()
+turtle.goto(500,-200)
+turtle.penup()
+turtle.goto(0,-300)
+turtle.pendown()
+turtle.goto(500,-300)
+turtle.penup()
+turtle.goto(0,-400)
+turtle.pendown()
+turtle.goto(500,-400)
+
+================================ RESTART: Shell ================================
+import turtle
+turtle.goto(100,100)
+turtle.undo
+<function undo at 0x000002044A2385E0>
+turtle.undo()
+turtle.goto(0,500)
+turtle.goto(500,500)
+turtle.goto(500,0)
+turtle.goto(0,0)
+
+turtle.goto(0,100)
+turtle.goto(0,500)
+turtle.goto(0,500)
+turtleundo
+Traceback (most recent call last):
+  File "<pyshell#41>", line 1, in <module>
+    turtleundo
+NameError: name 'turtleundo' is not defined
+turtle.undo
+<function undo at 0x000002044A2385E0>
+turtle.undo()
+turtle.undo()
+turtle.goto(500,100)
+turtle.goto(500,200)
+turtle.goto(0,200)
+turtle.goto(0,300)
+turtle.goto(500,300)
+turtle.goto(500,400)
+turtle.goto(0,400)
+turtle.goto(0,0)
+turtle.goto(100,0)
+turtle.goto(100,500)
+turtle.goto(200,500)
+turtle.goto(200,0)
+turtle.goto(300,0)
+turtle.goto(300,500)
+turtle.goto(400,500)
+turtle.goto(400,0)
