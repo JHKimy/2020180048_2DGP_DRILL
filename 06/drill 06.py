cfdac432b49a2edd2faefba0f5002b27e0ed37e9
Kim Jun Hyun <junyf00300@gmail.com>
2022-09-21 13:04:59 +0900

    이름 수정
    파일 이름을 바꿔서 올렸습니다.
    쓸모없는 파일에 drill 06으로 되어있어 바꾸었습니다.

--- 06/drill 06.py
+++ 06/drill 06.py
@@ -1,3 +1,5 @@
+import math
+
 from pico2d import *
 
 open_canvas()
@@ -5,28 +7,54 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('character.png')
 
-x=0
-y=0
-while(1):
-    while(x<800):
+while True:
+    x=0
+    while (x<800):
         clear_canvas_now()
         grass.draw_now(400,30)
         character.draw_now(x,90)
         x=x+2
         delay(0.001)
 
-    while(y<600):    
+    y=90
+    while(y<600):
+        clear_canvas_now()
+        grass.draw_now(400,30)
+        character.draw_now(800,y)
         y=y+2
         delay(0.001)
 
-    while(x>0):    
+    x=800
+    while (x>0):
+        clear_canvas_now()
+        grass.draw_now(400,30)
+        character.draw_now(x,600)
         x=x-2
         delay(0.001)
 
-    while(y>0):    
+    y=600
+    while (y>90):
+        clear_canvas_now()
+        grass.draw_now(400,30)
+        character.draw_now(0,y)
         y=y-2
         delay(0.001)
 
 
+    x=400
+    y=300
+    z=0
+    r=200
+
+    while (z<360):
     
+        clear_canvas_now()
+        grass.draw_now(400,30)
+        x= r*math.cos(z)+400
+        y=r*math.sin(z)+300
+        z=z+2
+        character.draw_now(x,y)
+        delay(0.001)
+
+
 close_canvas()
