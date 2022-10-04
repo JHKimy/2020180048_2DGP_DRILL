80e5fa0cfc3acf01e8b170b872751d873af65300
Kim Jun Hyun <junyf00300@gmail.com>
2022-10-04 18:36:05 +0900

    01
    리소스와 캐릭터 구현
    
    - 캐릭터 오류 수정해야함

--- project/2D_GAME/anim_viewer.py
+++ project/2D_GAME/anim_viewer.py
@@ -63,18 +63,18 @@ def handle_events():
 
 open_canvas(TUK_WIDTH, TUK_HEIGHT)
 TUK_GROUND = load_image('TUK_GROUND.png')
-character = load_image('animation_sheet.png')
+character = load_image('mario55.png')
 running = True
 
 while running:
         clear_canvas()
         TUK_GROUND.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
-        character.clip_draw(frame * 100, state * 100, 100, 100, x, y)
+        character.clip_draw(frame * 100, state * 100, 100, 100, x, y,150,150)
         update_canvas()
         handle_events()
-        frame = (frame + 1) % 8
-        x += dirX * 5
-        y += dirY * 5
+        frame = (frame + 1) % 3
+        x += dirX * 10
+        y += dirY * 10
         delay(0.01)
 
         if (x > TUK_WIDTH or y > TUK_HEIGHT-90 or x < 0 or y < 90):
